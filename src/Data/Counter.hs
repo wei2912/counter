978a063516be9e78ea17c0564d383573706e862a
Wei En <wei2912.supp0rt@gmail.com>
2014-12-04 16:59:54 +0800

    Document Counter.

--- src/Data/Counter.hs
+++ src/Data/Counter.hs
@@ -1,32 +1,67 @@
-module Data.Counter.Frac where
+module Data.Counter where
 
 import qualified Data.Map.Strict as M
 import qualified Data.List as L
 
+{-|
+  The Counter type is an alias
+  of Data.Map.Strict.Map.
+-}
 type Counter k v = M.Map k v
 
--- Initialize a counter with no keys.
+{-|
+  Initialize a counter with no keys.
+-}
 empty :: Counter k v
 empty = M.empty
 
--- Initialize a counter with a single
--- instance of a key.
-singleton :: Num v => k -> Counter k v
+{-|
+  Initialize a counter with a single
+  instance of a key.
+-}
+singleton ::
+	Num v
+    => k -- ^ Key to be inserted.
+    -> Counter k v -- ^ New counter.
 singleton k = M.singleton k 1
 
--- Increment the frequency count of a key
--- with a specified value.
-updateWith :: (Ord k, Num v) => k -> v -> Counter k v -> Counter k v
+{-|
+  Increment the frequency count of a key
+  with a specified value.
+-}
+updateWith ::
+	(Ord k, Num v)
+	=> k -- ^ Key to be inserted.
+	-> v -- ^ Specified frequency count.
+	-> Counter k v -- ^ Old counter.
+	-> Counter k v -- ^ New counter.
 updateWith = M.insertWith (+)
 
--- Increment the frequency count of a key by 1.
-increment :: (Ord k, Num v) => k -> Counter k v -> Counter k v
-increment k = updateWith k 1
+{-|
+  Increment the frequency count of a key by 1.
+-}
+update ::
+	(Ord k, Num v)
+	=> k -- ^ Key to be inserted.
+	-> Counter k v -- ^ Old counter.
+	-> Counter k v -- ^ New counter.
+update k = updateWith k 1
 
--- Convert a list of items into a counter.
-count :: (Ord k, Num v) => [k] -> Counter k v
-count = L.foldl' (flip increment) M.empty
+{-|
+  Convert a list of items into a counter.
+-}
+count ::
+	(Ord k, Num v)
+	=> [k] -- ^ List of keys.
+	-> Counter k v -- ^ New counter.
+count = L.foldl' (flip update) M.empty
 
--- Returns the union of two counters.
-union :: (Ord k, Num v) => Counter k v -> Counter k v -> Counter k v
+{-|
+  Returns the union of two counters.
+-}
+union ::
+	(Ord k, Num v)
+	=> Counter k v -- ^ First counter.
+	-> Counter k v -- ^ Second counter.
+	-> Counter k v -- ^ Union of both counters.
 union = M.unionWith (+)
