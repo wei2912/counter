af594e618252edaf6218b324c463e8ef7bbcb4af
Wei En <wei2912.supp0rt@gmail.com>
2014-12-11 22:38:43 +0800

    Fix mixed indentation.

--- src/Data/Counter.hs
+++ src/Data/Counter.hs
@@ -20,7 +20,7 @@ empty = M.empty
   instance of a key.
 -}
 singleton ::
-	Num v
+    Num v
     => k -- ^ Key to be inserted.
     -> Counter k v -- ^ New counter.
 singleton k = M.singleton k 1
@@ -30,38 +30,38 @@ singleton k = M.singleton k 1
   with a specified value.
 -}
 updateWith ::
-	(Ord k, Num v)
-	=> k -- ^ Key to be inserted.
-	-> v -- ^ Specified frequency count.
-	-> Counter k v -- ^ Old counter.
-	-> Counter k v -- ^ New counter.
+    (Ord k, Num v)
+    => k -- ^ Key to be inserted.
+    -> v -- ^ Specified frequency count.
+    -> Counter k v -- ^ Old counter.
+    -> Counter k v -- ^ New counter.
 updateWith = M.insertWith (+)
 
 {-|
   Increment the frequency count of a key by 1.
 -}
 update ::
-	(Ord k, Num v)
-	=> k -- ^ Key to be inserted.
-	-> Counter k v -- ^ Old counter.
-	-> Counter k v -- ^ New counter.
+    (Ord k, Num v)
+    => k -- ^ Key to be inserted.
+    -> Counter k v -- ^ Old counter.
+    -> Counter k v -- ^ New counter.
 update k = updateWith k 1
 
 {-|
   Convert a list of items into a counter.
 -}
 count ::
-	(Ord k, Num v)
-	=> [k] -- ^ List of keys.
-	-> Counter k v -- ^ New counter.
+    (Ord k, Num v)
+    => [k] -- ^ List of keys.
+    -> Counter k v -- ^ New counter.
 count = L.foldl' (flip update) M.empty
 
 {-|
   Returns the union of two counters.
 -}
 union ::
-	(Ord k, Num v)
-	=> Counter k v -- ^ First counter.
-	-> Counter k v -- ^ Second counter.
-	-> Counter k v -- ^ Union of both counters.
+    (Ord k, Num v)
+    => Counter k v -- ^ First counter.
+    -> Counter k v -- ^ Second counter.
+    -> Counter k v -- ^ Union of both counters.
 union = M.unionWith (+)
